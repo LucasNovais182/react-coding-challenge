f8c5ee27a32250d6857ffb68d31c3f72a02ff729
Lucas <lucas.novais21@outlook.com>
2020-12-20 01:06:44 -0300

    create header component

---
+++ stan-coding-challenge/src/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+import '../global.css'
+
+const index = () => {
+    return (
+        <header>
+            <h1>DEMO Streaming</h1>
+
+            <nav>
+                <ul>
+                    <li>Log in</li>
+                    <li>Start your free trial</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default index
